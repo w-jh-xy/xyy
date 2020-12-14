30a81512210a51492bd814436770f0a516f4fcb4
w-jh-xy <1599681172@qq.com>
2020-12-14 23:29:00 +0800

    14

---
+++ my code/test12_14_1/test_12_14_1.c
@@ -0,0 +1,37 @@
+//#include<stdio.h>
+//int bubble(int arr[], int sz)
+//{
+//	int i = 0;//趟数=sz-1
+//	int j = 0;//每趟的比较次数=sz-1-i
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] < arr[j + 1])
+//			{
+//				int tmp = 0;
+//				tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int len = sizeof(arr) / sizeof(arr[0]);
+//	bubble(arr, len);
+//	for (int x = 0; x < len; x++)
+//	{
+//		printf("%d ", arr[x]);
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
